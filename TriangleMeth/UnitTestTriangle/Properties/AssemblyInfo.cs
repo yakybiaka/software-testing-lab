2086c416825e14e730deb445092d1870938e29b6
ykybiaka <yakybiaka40@gmail.com>
2018-09-30 00:42:01 +0300

    lab xUnit

---
+++ TriangleMeth/UnitTestTriangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTriangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTriangle")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3afaafdc-e829-44a6-bbd1-4b63e506102d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriangleMeth/UnitTestTriangle/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTestTriangle
+{
+    [TestClass]
+    class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           // Assert.Equals(false, TriangleCheck.);
+          
+            int a = -5;
+            int b = 3;
+            int c = 5;
+            bool s = false;
+
+            //TriangleCheck second = new TriangleCheck();
+          //  bool actual = second.test(a, b, c);
+
+            Assert.AreEqual(s, actual);
+        }
+    }
+}
