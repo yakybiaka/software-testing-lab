668e6909fbbee4c61c894b8f8db8a2cee4cfa9cd
ykybiaka <yakybiaka40@gmail.com>
2018-09-30 23:05:24 +0300

    new test meth

--- Triangle Check Method/CheckTriangle/CheckTriangleTests/TriangleTests.cs
+++ Triangle Check Method/CheckTriangle/CheckTriangleTests/TriangleTests.cs
@@ -85,6 +85,17 @@ namespace CheckTriangleTests
         }
 
         [TestMethod]
+        public void Test_x_double_y_double_return_true()
+        {
+            //act
+            TriangleCheck tr = new TriangleCheck();
+            bool actual = tr.Check_Triangle(6.02, 7.00004, 10);
+
+            //assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
         public void Test_string_y_ConvertToDouble_return_true()
         {
             //act
