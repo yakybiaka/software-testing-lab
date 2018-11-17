b1fcb190eafd9693236cd3e054054805ed773995
ykybiaka <yakybiaka40@gmail.com>
2018-11-18 00:13:22 +0300

    Framework Tests

---
+++ FrameworkTest/Framework/Driver/DriverInstance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Framework.Driver
+{
+    public class DriverInstance
+    {
+        private static IWebDriver driver;
+
+        private DriverInstance() { }
+
+        public static IWebDriver GetInstance()
+        {
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(30));
+                driver.Manage().Window.Maximize();
+            }
+            return driver;
+        }
+
+        public static void CloseBrowser()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }    
+}
---
+++ FrameworkTest/Framework/Steps/Steps.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Framework.Steps
+{
+    class Steps
+    {
+        IWebDriver driver;
+
+        public void InitBrowser()
+        {
+            driver = Driver.DriverInstance.GetInstance();
+        }
+
+        public void CloseBrowser()
+        {
+            Driver.DriverInstance.CloseBrowser();
+        }
+
+        public void EnterCities(string cityOfDeparture, string cityOfArrival)
+        {
+            Pages.MainPage mainPage = new Pages.MainPage(driver);
+            mainPage.OpenPage();
+            mainPage.EnterCityOfDeparture(cityOfDeparture);
+            mainPage.EnterCityOfArrival(cityOfArrival);
+            mainPage.EnterDepartureDate("17.11.2018");
+            mainPage.EnterDepartureBackDate("19.11.2018");
+        }
+    }
+}
